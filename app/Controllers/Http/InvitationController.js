cd806e0950cea2c198986c7fbfec9bd108b4db59
obeza <o.bezaudin@gmail.com>
2017-11-21 21:47:19 -0400

    first commit

---
+++ app/Controllers/Http/InvitationController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const view = use('View')
+
+class InvitationController {
+  async index () {
+    return view.render('pages.invitation.index')
+  }
+
+  async create () {
+  }
+
+  async store () {
+  }
+
+  async show () {
+  }
+
+  async edit () {
+  }
+
+  async update () {
+  }
+
+  async delete () {
+  }
+}
+
+module.exports = InvitationController
